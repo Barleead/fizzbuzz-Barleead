69e6fefca00dadc3386acfc4e9b0d07f8a22d5fb
Barbara Adkins <142528866+Barleead@users.noreply.github.com>
2023-10-20 16:18:00 -0400

    Update fizzbuzz.py
    bonus challenge

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,2 +1,11 @@
-# add your code here
+//fizzbuzz bonus challenge
+for x in range(1, 101):
+	if x % 3 == 0 and x % 5 == 0:
+  		print('fizzbuzz')
+	elif x % 5 == 0:
+  		print('buzz')  
+	elif x % 3 == 0:
+  		print('fizz')
+	else:
+  		print(x)# add your code here
 
